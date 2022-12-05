354507ca2dc78a9fca653a953926431d1ed67c03
Slava <Slava.lokshin@mail.ru>
2022-12-05 18:12:46 +0300

    Поправил линтер еще раз

--- src/client/internal/include/mainwindow.hpp
+++ src/client/internal/include/mainwindow.hpp
@@ -1,22 +1,21 @@
-#ifndef MAINWINDOW_H
-#define MAINWINDOW_H
+#pragma once
 
 #include <QMainWindow>
 
 QT_BEGIN_NAMESPACE
-namespace Ui { class MainWindow; }
+namespace Ui {
+class MainWindow;
+}
+
 QT_END_NAMESPACE
 
-class MainWindow : public QMainWindow
-{
+class MainWindow : public QMainWindow {
     Q_OBJECT
 
-public:
-    MainWindow(QWidget *parent = nullptr);
+ public:
+    explicit MainWindow(QWidget *parent = nullptr);
     ~MainWindow();
 
-private:
+ private:
     Ui::MainWindow *ui;
 };
-#endif // MAINWINDOW_H
-
